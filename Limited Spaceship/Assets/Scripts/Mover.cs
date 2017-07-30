e48b8093e1c2a874de68ddef15318264123d1ad5
aneudissalcedo <aneudissalcedo@gmail.com>
2017-07-30 05:33:29 -0700

    Added player and player movement with laser

---
+++ Limited Spaceship/Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour 
+{
+	[SerializeField]private float speed;
+	private new Rigidbody rigidbody;
+
+	void Start()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+		rigidbody.velocity = transform.forward * speed;
+	}
+}
